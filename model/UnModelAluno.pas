f8577fc8b50b5c07293d42c520f6d1ab89af632e
sergioafarciere <sergio.scar56@gmail.com>
2019-02-08 11:56:59 -0300

    Criação das classes

---
+++ model/UnModelAluno.pas
@@ -0,0 +1,16 @@
+unit UnModelAluno;
+
+interface
+
+uses UnModelAgente;
+
+type TAluno = class(TAgente)
+     public
+
+     end;
+
+implementation
+
+
+
+end.
---
+++ model/UnModelEscola.pas
@@ -0,0 +1,24 @@
+unit UnModelEscola;
+
+interface
+
+uses UnModelAgente;
+
+type TEscola = class(TAgente)
+     private
+       { private declarations }
+     protected
+       { protected declarations }
+     public
+       { public declarations }
+       
+     published
+       { published declarations }
+     end;
+
+
+implementation
+
+
+
+end.
