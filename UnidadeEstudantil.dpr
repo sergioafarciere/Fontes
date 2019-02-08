b7c91df726f29f2fb4ff72f6c2d11e535a654482
sergioafarciere <sergio.scar56@gmail.com>
2019-02-08 13:16:27 -0300

    master

--- UnidadeEstudantil.dpr
+++ UnidadeEstudantil.dpr
@@ -2,13 +2,22 @@ program UnidadeEstudantil;
 
 uses
   Forms,
-  UnFrmPrincipal in 'view\UnFrmPrincipal.pas' {Form1};
+  UnFrmPrincipal in 'view\UnFrmPrincipal.pas' {frmPrincipal},
+  UnModelAgente in 'model\UnModelAgente.pas',
+  UnModelAluno in 'model\UnModelAluno.pas',
+  UnModelEscola in 'model\UnModelEscola.pas',
+  UnFrmPai in 'view\UnFrmPai.pas' {frmPai},
+  UnFrmCadAluno in 'view\UnFrmCadAluno.pas' {frmCadAluno},
+  UnFrmCadEscola in 'view\UnFrmCadEscola.pas' {frmCadEscola};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TfrmPai, frmPai);
+  Application.CreateForm(TfrmCadAluno, frmCadAluno);
+  Application.CreateForm(TfrmCadEscola, frmCadEscola);
   Application.Run;
 end.
---
+++ view/UnFrmCadEscola.pas
@@ -0,0 +1,24 @@
+unit UnFrmCadEscola;
+
+interface
+
+uses
+  UnFrmPai, Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TfrmCadEscola = class(TfrmPai)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmCadEscola: TfrmCadEscola;
+
+implementation
+
+{$R *.dfm}
+
+end.
